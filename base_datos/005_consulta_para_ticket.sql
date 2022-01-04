a27374997c0f4d08888c8fa842405846da21fd4b
Bryan Hernandez <androbryaan@gmail.com>
2022-01-04 01:55:24 -0600

    Notas Para Corregir

--- base_datos/005_consulta_para_ticket.sql
+++ base_datos/005_consulta_para_ticket.sql
@@ -2,9 +2,13 @@ select * FROM sistema_escolar.cobros_caja;
 
 SELECT cobros_caja.idcobros_caja AS "# TICKET", 
 /*cobros_mensuales.Id_cobros_mensuales,*/ 
+/*DATOS ALUMNO*/
 datos_alumno.id_alumno AS "MATRICULA:", datos_alumno.nombre AS "NOMBRE DEL ALUMNO:", datos_alumno.apellido_paterno AS "APELLIDO PATERNO:", datos_alumno.apellido_materno AS "APELLIDO MATERNO",
+/*DATOS ESCOLARES*/
 escuela_plantel.Nombre AS "ESCUELA:", niveles_educativos.Nombre_nivel AS "NIVEL EDUCATIVO:",
+/*DATOS COBRANZAS*/
 conceptos_cobranza.Concepto AS "CONCEPTO:", conceptos_cobranza.Precio AS "PRECIO:",
+/*DATOS DE LA VENTA*/
 cobros_caja.Fecha AS "FECHA:", cobros_caja.Descuentos AS "DESCUENTOS:", cobros_caja.Recargos AS "RECARGOS:", cobros_caja.Metodo_Pago AS "METODO DE PAGO:", cobros_caja.Total AS "TOTAL:"
 FROM sistema_escolar.cobros_caja
 INNER JOIN cobros_mensuales ON cobros_caja.id_cobros_mensuales = cobros_mensuales.Id_cobros_mensuales
@@ -12,7 +16,7 @@ INNER JOIN cobros_alumno ON cobros_mensuales.id_cobros_alumno = cobros_alumno.id
 INNER join conceptos_cobranza ON cobros_alumno.conceptos_cobranza_id_Folio_cobranza = conceptos_cobranza.id_Folio_cobranza
 INNER JOIN servicio_escolar ON cobros_alumno.id_servicio_escolar = servicio_escolar.id_servicio_escolar
 INNER JOIN datos_alumno ON servicio_escolar.id_alumno = datos_alumno.id_alumno
-INNER JOIN planteles_niveles ON servicio_escolar.id_clave_programa_educativo = planteles_niveles.clave_programa_educativo /* TABLA PADRE, SE DECLARA PARA QUE PODAMOS OBTENER INFORMACION DE LAS HIJAS*/
-INNER JOIN escuela_plantel ON planteles_niveles.plantel_id_plantel = escuela_plantel.id_plantel /* TABLA HIJA de planteles_niveles*/
+INNER JOIN planteles_niveles ON servicio_escolar.id_clave_programa_educativo = planteles_niveles.clave_programa_educativo 
+INNER JOIN escuela_plantel ON planteles_niveles.plantel_id_plantel = escuela_plantel.id_plantel 
 INNER JOIN niveles_educativos ON planteles_niveles.niveles_educativos_id_nivel = niveles_educativos.id_nivel
-Where  cobros_mensuales.Id_cobros_mensuales = 5122; /* TABLA HIJA de planteles_niveles*/
+Where  cobros_mensuales.Id_cobros_mensuales = 5122; 
