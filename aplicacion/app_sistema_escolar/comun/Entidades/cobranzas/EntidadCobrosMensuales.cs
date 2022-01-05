6a36d216175fe7a3fa186a04216cbbf3e4490a32
Alberto C <alberto78.cp@gmail.com>
2022-01-05 14:33:44 -0600

    Arreglados conflictos

--- aplicacion/app_sistema_escolar/comun/Entidades/cobranzas/EntidadCobrosMensuales.cs
+++ aplicacion/app_sistema_escolar/comun/Entidades/cobranzas/EntidadCobrosMensuales.cs
@@ -8,9 +8,9 @@ namespace comun.Entidades.cobranzas
 {
     public class EntidadCobrosMensuales
     {
-        public int IdCobrosMensuales { get; set; }
-        public int IdCobrosAlumno{ get; set; }
+        public int IdCobrosAlumno { get; set; }
         public string MesPago { get; set; }
         public string Estatus { get; set; }
+
     }
 }
