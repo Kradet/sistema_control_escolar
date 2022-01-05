40bcd345fcfd1a2a6f8ad6799650a331944ddb5a
Bryan Hernandez <androbryaan@gmail.com>
2022-01-05 16:47:55 -0600

    Añadido Formulario del Semaforo

---
+++ aplicacion/app_sistema_escolar/app_sistema_escolar/fmr_SemaforoVigilante.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace app_sistema_escolar
+{
+    public partial class fmr_SemaforoVigilante : Form
+    {
+        public fmr_SemaforoVigilante()
+        {
+            InitializeComponent();
+        }
+        
+        private void panelSemaforo_Paint(object sender, PaintEventArgs e)
+        {
+
+
+        }
+        
+    }
+}
